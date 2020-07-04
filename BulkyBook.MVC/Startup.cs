724d72f1dd67dce41f61e04d17129f9c31913ea2
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-07-04 13:18:21 +0200

    Section 4 - Install Razor RunTime Compilation

--- BulkyBook.MVC/Startup.cs
+++ BulkyBook.MVC/Startup.cs
@@ -30,9 +30,9 @@ namespace BulkyBook.MVC
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
 
