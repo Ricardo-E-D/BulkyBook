e967055f7f42ceeb6ee3bee1053a6f39a52e8f36
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-07-04 13:32:16 +0200

    Section 5 - Separating out Data Layer moved the data folder to data access layer

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyBook.MVC.Data
+namespace BulkyBook.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
