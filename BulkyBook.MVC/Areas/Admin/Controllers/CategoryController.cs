ea3061f88d255dd44474f53e3e121bb60ad387ce
Ricardo-E-D <47738496+Ricardo-E-D@users.noreply.github.com>
2020-07-10 13:23:08 +0200

    Section 7 - Category Index and Get All Actions categories.getall is called by an [httpget] method rather than index, due to the usage of datatables

--- BulkyBook.MVC/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook.MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.MVC.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        #endregion
     }
 }
